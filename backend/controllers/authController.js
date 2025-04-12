dacb1ea4f0e88f9b9a2c0bb080c068327fc83e88
Alok <advancedtechnology62@gmail.com>
2025-04-12 13:20:17 +0530

    auth-signup/login

---
+++ backend/controllers/authController.js
@@ -0,0 +1,80 @@
+const User = require('../models/User');
+const generateToken = require('../utils/generatetoken');
+
+// Signup API
+const signupUser = async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+
+    if (!username?.trim() || !email?.trim() || !password?.trim()) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    const user = await User.create({
+      username: username.trim(),
+      email: email.trim(),
+      password,
+    });
+
+    const token = generateToken(user._id);
+
+    res.status(201).json({
+      success: true,
+      message: "Signup successful",
+      token,
+      user: {
+        _id: user._id,
+        username: user.username,
+        email: user.email,
+      },
+    });
+
+  } catch (error) {
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+
+// Login API
+const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email?.trim() || !password?.trim()) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    const isMatch = await user.matchPassword(password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid Credentials" });
+    }
+
+    const token = generateToken(user._id);
+
+    res.status(200).json({
+      success: true,
+      message: "Login successful",
+      token,
+      user: {
+        _id: user._id,
+        username: user.username,
+        email: user.email,
+      },
+    });
+
+  } catch (error) {
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+module.exports = { signupUser, loginUser };
