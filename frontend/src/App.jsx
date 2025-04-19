d7b009d44f83e5ff83431a8e181f6337e2cc46a8
Alok <advancedtechnology62@gmail.com>
2025-04-19 22:35:08 +0530

    frontend-component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Navbar from './components/Navbar';
-// import AppRoutes from './routes/AppRoutes';
+import AppRoutes from './routes/AppRoutes';
 import './App.css';
 const App = () => (
   <Router>
     <Navbar />
     <div className="pt-16"> 
-      {/* <AppRoutes /> */}
+      <AppRoutes />
     </div>
   </Router>
 );
